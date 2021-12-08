b6cc55d7a73d19cf1bf652dee4f336d742cc154f
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-12-08 22:52:44 +0100

    detailVC under contruction

---
+++ PurpleImage/Custom Views/PiBodyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  PiBodyLabel.swift
+//  PurpleImage
+//
+//  Created by Frederico Kuckelhaus on 08.12.21.
+//
+
+import UIKit
+
+class PiBodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PurpleImage/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  PurpleImage
+//
+//  Created by Frederico Kuckelhaus on 08.12.21.
+//
+
+import Foundation
